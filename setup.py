76ed25ba6c7bd9ca83b07a274b1e32fadee57a2a
nnanos <nnanos@ceid.uptartas.gr>
2022-02-19 21:23:25 +0200

    last commit

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name='Audio-proc-lib',
-    version='0.0.3',
+    version='0.0.4',
     
     license='MIT',
     description='A library that contains basic audio signal processing functionalities',
